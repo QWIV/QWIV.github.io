cccf52c4b4e7e4a27f08148d2938d7a18da552d9
QWIV <qwu@iesvallvera.cat>
2024-04-10 09:38:54 +0200

    e

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const submit = document.getElementById("submi-button");
     const form = document.getElementById("form");
     form.addEventListener("submit", (form) => {
         console.log(form);
